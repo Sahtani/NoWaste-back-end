5fd676b65ddf8133021a58c998672683247dce48
Sahtani <soumia.sahtani@gmail.com>
2025-01-13 12:03:37 +0100

    init project

---
+++ src/main/java/com/youcode/nowastebackend/NoWasteBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.youcode.nowastebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoWasteBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoWasteBackEndApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/youcode/nowastebackend/entity/User.java
@@ -0,0 +1,4 @@
+package com.youcode.nowastebackend.entity;
+
+public class User {
+}
